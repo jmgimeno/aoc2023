ef4ba3f8e5e7838669065c65a5e6089a47339810
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2023-12-02 07:15:50 +0100

    updated comment

--- days/src/main/java/aoc2023/day2/Day2.java
+++ days/src/main/java/aoc2023/day2/Day2.java
@@ -82,16 +82,16 @@ public class Day2 {
         }
 
         static Game parse(String line) {
-            // parses using a regular expression the line and returns the total number of cubes if
-            // each color
-            // that are present in the lines with the format:
+            // parses using a regular expression the line and returns the maximum number of cubes if
+            // each color that are present in the line
+            // Lines have the format:
             //    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
             //    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
             //    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
             //    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
             //    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
             // the result, for the first one, would be:
-            // RED -> 5, GREEN -> 3, BLUE -> 11
+            //    RED -> 4, GREEN -> 2, BLUE -> 6
 
             Map<Color, Integer> counters = new HashMap<>();
             String regex = "Game (\\d+):|((\\d+)\\s+(blue|red|green))";
