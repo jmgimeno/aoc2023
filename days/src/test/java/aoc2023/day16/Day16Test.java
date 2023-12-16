7e7af1a0fc6a64d3c2daa9d091175c5c7beb5e8b
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2023-12-16 14:43:13 +0100

    created day 16

---
+++ days/src/test/java/aoc2023/day16/Day16Test.java
@@ -0,0 +1,48 @@
+package aoc2023.day16;
+
+import aoc2023.utils.IO;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day16Test {
+
+    static final String example = """
+            """;
+
+    static final Day16 day16 = new Day16();
+
+    @Test
+    @DisplayName("part1 - example data")
+    @Disabled("part1 - not implemented")
+    void test1() {
+        var data = IO.splitLinesAsList(example);
+        assertEquals(-1, day16.part1(data));
+    }
+
+    @Test
+    @DisplayName("part1 - input data")
+    @Disabled("part1 - not implemented")
+    void test2() {
+        var data = IO.getResourceAsList("day16.txt");
+        assertEquals(-1, day16.part1(data));
+    }
+
+    @Test
+    @DisplayName("part2 - example data")
+    @Disabled("part2 - not implemented")
+    void test3() {
+        var data = IO.splitLinesAsList(example);
+        assertEquals(-1, day16.part2(data));
+    }
+
+    @Test
+    @DisplayName("part2 - input data")
+    @Disabled("part2 - not implemented")
+    void test4() {
+        var data = IO.getResourceAsList("day16.txt");
+        assertEquals(-1, day16.part2(data));
+    }
+}
