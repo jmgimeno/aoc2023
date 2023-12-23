fca5f17d95551f36e9c2f74e050814b164a5fb05
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2023-12-23 09:18:55 +0100

    updated CharGrid

--- days/src/main/java/aoc2023/utils/CharGrid.java
+++ days/src/main/java/aoc2023/utils/CharGrid.java
@@ -8,13 +8,22 @@ public class CharGrid {
     protected final char[][] points;
 
     public CharGrid(List<String> data) {
-        height = data.size();
-        width = data.get(0).length();
-        points = new char[height + 2][width + 2];
-        fillPoints(data);
+        this(data, true);
     }
 
-    private void fillPoints(List<String> data) {
+    public CharGrid(List<String> data, boolean embed) {
+        height = data.size();
+        width = data.getFirst().length();
+        if (embed) {
+            points = new char[height + 2][width + 2];
+            fillPointsEmbed(data);
+        } else {
+            points = new char[height][width];
+            fillPointsNotEmbed(data);
+        }
+    }
+
+    private void fillPointsEmbed(List<String> data) {
         for (int y = 0; y < height + 2; y++) {
             for (int x = 0; x < width + 2; x++) {
                 if (x == 0 || y == 0 || x == width + 1 || y == height + 1) {
@@ -22,6 +31,14 @@ public class CharGrid {
                 } else {
                     points[y][x] = data.get(y - 1).charAt(x - 1);
                 }
+            }
+        }
+    }
+
+    private void fillPointsNotEmbed(List<String> data) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                points[y][x] = data.get(y).charAt(x);
             }
         }
     }
