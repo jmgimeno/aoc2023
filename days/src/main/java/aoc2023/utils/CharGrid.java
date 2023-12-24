e615b64a4f539cfebaf18df62025557f3954a954
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2023-12-24 10:50:04 +0100

    updated data grid to choose embedding character

--- days/src/main/java/aoc2023/utils/CharGrid.java
+++ days/src/main/java/aoc2023/utils/CharGrid.java
@@ -11,23 +11,31 @@ public class CharGrid {
         this(data, true);
     }
 
+    public CharGrid(List<String> data, char embedding) {
+        this(data, true, embedding);
+    }
+
     public CharGrid(List<String> data, boolean embed) {
+        this(data, embed, '.');
+    }
+
+    public CharGrid(List<String> data, boolean embed, char embedding) {
         height = data.size();
         width = data.getFirst().length();
         if (embed) {
             points = new char[height + 2][width + 2];
-            fillPointsEmbed(data);
+            fillPointsEmbed(data, embedding);
         } else {
             points = new char[height][width];
             fillPointsNotEmbed(data);
         }
     }
 
-    private void fillPointsEmbed(List<String> data) {
+    private void fillPointsEmbed(List<String> data, char embedding) {
         for (int y = 0; y < height + 2; y++) {
             for (int x = 0; x < width + 2; x++) {
                 if (x == 0 || y == 0 || x == width + 1 || y == height + 1) {
-                    points[y][x] = '.';
+                    points[y][x] = embedding;
                 } else {
                     points[y][x] = data.get(y - 1).charAt(x - 1);
                 }
